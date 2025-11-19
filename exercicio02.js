43b69620a095dcf5e73a2a956138065de57af6a4
FELIPE FERREIRA DA GLÓRIA VASCONCELOS <49808054854@imaccommand016.local>
2025-11-19 11:51:37 -0300

    Exercicio02

---
+++ exercicio02.js
@@ -0,0 +1,39 @@
+let diadasemana = prompt("Digiti o dia da semana")
+
+switch (diadasemana) {
+    case "1":
+        console.log("Segunda-Feira")
+        break;
+
+    case "2":
+        console.log("Terça-Feira")
+        break;
+
+    case "3":
+        console.log("Quarta-Feira")
+        break;
+
+    case "4":
+        console.log("Quinta-Feira")
+        break;
+
+        case "4":
+        console.log("Quinta-Feira")
+        break;
+
+        case "5":
+        console.log("Sexta-Feira")
+        break;
+
+        case "6":
+        console.log("Sabado")
+        break;
+
+        case "7":
+        console.log("Domingo")
+        break;
+
+    default:
+        console.log("Não existe esse dia!!!")
+        break;
+}
