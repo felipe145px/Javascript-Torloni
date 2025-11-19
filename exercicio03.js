ff2641e72e901a80a2a3678e5b43b0df7cc7e065
FELIPE FERREIRA DA GLÓRIA VASCONCELOS <49808054854@imaccommand016.local>
2025-11-19 13:38:37 -0300

    exercicio03

---
+++ exercicio03.js
@@ -0,0 +1,39 @@
+let imparoupar = prompt("Ímpar ou Par")
+
+switch (imparoupar) {
+    case "1":
+        console.log("Ímpar")
+        break;
+
+    case "2":
+        console.log("Par")
+        break;
+
+    case "3":
+        console.log("Ímpar")
+        break;
+
+    case "4":
+        console.log("Par")
+        break;
+
+        case "5":
+        console.log("Ímpar")
+        break;
+
+        case "6":
+        console.log("Par")
+        break;
+
+        case "7":
+        console.log("Ímpar")
+        break;
+
+        case "8":
+        console.log("Par")
+        break;
+
+    default:
+        console.log("Não existe")
+        break;
+}
