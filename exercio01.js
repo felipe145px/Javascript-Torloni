b4925292ccf51a65ad5103562f537eeac24f2f03
FELIPE FERREIRA DA GLÓRIA VASCONCELOS <49808054854@imaccommand016.local>
2025-11-19 11:31:51 -0300

    Exercio01

---
+++ exercio01.js
@@ -0,0 +1,22 @@
+let nome = (prompt("Qual o seu nome?"))
+
+ if (nome == "admin") {
+     console.log("Acesso Liberado")
+ }
+else{
+     console.log("Acesso Negado")
+}
+
+
+
+
+
+
+let senha = (prompt("Qual a sua Senha"))
+
+ if (senha == "senha123") {
+     console.log("Acesso Liberado")
+ }
+else{
+     console.log("Acesso Negado")
+}
