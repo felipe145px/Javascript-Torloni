b8f02ccd0a77358f5662ad86aad21243fe51a61a
FELIPE FERREIRA DA GLÓRIA VASCONCELOS <49808054854@imaccommand016.local>
2025-12-05 08:17:14 -0300

    finalizado

---
+++ for.js
@@ -0,0 +1,24 @@
+// sintaxe = escrita do código
+
+// for - laço de repetição = loop
+// laço de repetição tem voltas
+
+// laço contdo - quando sabems quantas voltas
+// laço condicional - irá fazer as voltas se a condição for vedadeira
+
+// for([inicialização]; [comdição]; [incremento]){
+
+// }
+// a variavel i vale 0 ou seja essa varialvel armazena as voltas
+// a condicao verifica o numero que o loop está
+for(let i = 0; i <= 9; i = i + 1){
+    console.log("A volta que for está é: "+ i);
+
+}
+
+// utilize o for para exibir do 1 a 10
+
+for(let i =1; i <= 10; i = i + 1){
+    console.log("A volta que for está é: "+ i);
+
+}
